1b9c4437ce44b17245022a6a945fe991e9fbde22
Martin <martin@kiranatama.com>
2020-01-04 01:52:59 +0700

    added validation for contact_us

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ApplicationRecord
+  validates :name, :presence => true
+  validates :email, :presence => true
+  validates :phone, :presence => true
+  validates :content, :presence => true
 end
