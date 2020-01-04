5cbb92dc857964d9413d0e92e8e1cd5b7655c6df
Martin <martin@kiranatama.com>
2020-01-04 11:59:26 +0700

    fixed recaptcha config, fixed email content

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LcnGMwUAAAAANrfK_UfGk21qOJxErefSfIupaRH'
-  config.private_key = '6LcnGMwUAAAAAAjgyFIsTDoS4n3k36J7lwK9uLNJ'
+  config.public_key  = "#{Rails.application.secrets.recaptcha_public_key}"
+  config.private_key = "#{Rails.application.secrets.recaptcha_secret_key}"
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
